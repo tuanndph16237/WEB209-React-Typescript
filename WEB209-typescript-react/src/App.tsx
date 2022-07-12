0d23f317b05ff795d8666e0467c708fc1054e09d
tuanndph16237 <tuanndph16237@example.com>
2022-07-12 23:44:46 +0700

    lesson 3

--- WEB209-typescript-react/src/App.tsx
+++ WEB209-typescript-react/src/App.tsx
@@ -2,26 +2,27 @@ import { useState } from 'react'
 import logo from './logo.svg'
 import './App.css'
 import styled from 'styled-components'
-import 
-import { Route, Routes } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home/Home'
+import ProductDetail from './pages/ProductDetail/ProductDetail'
+import Header from './components/Header/Header'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <Container>
+      <Header/>
       <Routes>
-        <Route path='/' element={<Home/>}>
-
-        </Route>
+        <Route path="/" element={<Home />}></Route>
+        <Route path='/product' element={<ProductDetail />}></Route>
       </Routes>
     </Container>
   )
 }
 
 const Container = styled.div`
-    background-color: #282c34;
-`
 
+`
 
 export default App
